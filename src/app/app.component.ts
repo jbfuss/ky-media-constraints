c31942b91fa0e9899721f625ef6a1944c8824301
Jb Fuss <jbfuss.ext@kayentis.com>
2020-12-02 15:51:52 +0100

    Display media-constraints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ky-media-constraints';
+  innerWidth = window.innerWidth;
+  innerHeight = window.innerHeight;
+
+
+  @HostListener('window:resize', [])
+  public onResize(): void {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+  }
+
 }
